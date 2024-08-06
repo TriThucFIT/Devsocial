5292b0c01d61425acf18e95e5396aadbbef145ed
TriThucFIT <trithucbuittb@gmail.com>
2024-08-06 14:14:15 +0700

    init repositories

---
+++ authenticate/src/test/java/vn/edu/iuh/fit/authenticate/AuthenticateApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.authenticate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
